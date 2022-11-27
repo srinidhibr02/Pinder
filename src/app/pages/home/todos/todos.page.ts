fc0f4fc676d339b3366ff38679f296a6c0f14f78
zerolpa <zerolpa2022@gmail.com>
2022-11-27 22:59:43 +0530

    Todo Date implemented

--- src/app/pages/home/todos/todos.page.ts
+++ src/app/pages/home/todos/todos.page.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.page.scss'],
 })
 export class TodosPage implements OnInit {
-
+  today:Date = new Date();
+  currentWeek:Array<Date> = [];
+  isModalOpen = false;
+  
   constructor() { }
 
   ngOnInit() {
+    const now = new Date();
+    const startOfWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() - now.getDay());
+    for(let i=0;i<7;i++){
+      this.currentWeek.push(
+        new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + i)
+      );
+    }
   }
 
+  compareDatetoToday(date:Date){
+      return date.setHours(0,0,0,0) === new Date().setHours(0,0,0,0);
+  }
+
+
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
 }
